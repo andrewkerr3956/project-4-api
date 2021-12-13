98d207db7c000ca63ba891261d9d2e21d4af909f
Andrew Kerr <andrewkerr3956@gmail.com>
2021-12-12 22:50:01 -0500

    Built the API for the Yahoo Stocks, should be working okay, will be implemented into React.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var yahoo =  require('yahoo-stock-prices');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/api/search/:symbol', async(req, res) => {
+  try {
+    // res.set('Access-Control-Allow-Origin', '*');
+    const data = await yahoo.getCurrentData(req.params.symbol);
+    res.json({success: true, data: data});
+  } catch(e) {
+    console.log(e)
+    res.json({success: false, data:"Unsuccessful."});
+  }
 });
 
 module.exports = router;
