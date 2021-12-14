fa378a92113110efd9dfbcf66228ab783a301fad
Andrew Kerr <andrewkerr3956@gmail.com>
2021-12-13 22:51:28 -0500

    Started building the database portion of the api.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var yahoo =  require('yahoo-stock-prices');
+import { Sequelize, Model, DataType } from 'sequelize';
+const sequelize = new Sequelize('Users', 'root', 'root', {
+  dialect: 'mysql'
+}); 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,5 +21,10 @@ router.get('/api/search/:symbol', async(req, res) => {
     res.json({success: false, data:"Unsuccessful."});
   }
 });
+
+router.put('/api/users/:params', (req, res) => {
+  let query = encodeURI(req.params);
+  sequelize.query(`INSERT INTO Users (username, password) VALUES (${query.username}, ${query.password})`).then([results, metadata]);
+})
 
 module.exports = router;
