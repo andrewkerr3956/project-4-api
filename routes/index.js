48eb3e21981a7a916c0ce68c77a9b1124ace0edb
Andrew Kerr <andrewkerr3956@gmail.com>
2021-12-14 22:19:47 -0500

    Built the GET and POST routes for dealing with creating users and logging in users.

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,60 @@
 var express = require('express');
 var router = express.Router();
-var yahoo =  require('yahoo-stock-prices');
-import { Sequelize, Model, DataType } from 'sequelize';
-const sequelize = require('../lib/db.js');
+var yahoo = require('yahoo-stock-prices');
+const bcrypt = require('bcrypt');
+const mysql = require('../lib/db.js');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+  mysql.conn.connect();
 });
 
-router.get('/api/search/:symbol', async(req, res) => {
+router.get('/api/search/:symbol', async (req, res) => {
   try {
-    // res.set('Access-Control-Allow-Origin', '*');
     const data = await yahoo.getCurrentData(req.params.symbol);
-    res.json({success: true, data: data});
-  } catch(e) {
+    res.json({ success: true, data: data });
+  } catch (e) {
     console.log(e)
-    res.json({success: false, data:"Unsuccessful."});
+    res.json({ success: false, data: "Unsuccessful." });
   }
 });
 
-router.put('/api/users/:params', (req, res) => {
-  let query = encodeURI(req.params);
-  sequelize.query(`INSERT INTO Users (username, password) VALUES (${query.username}, ${query.password})`).then([results, metadata]);
-})
+// Checking if the credentials match when you login.
+router.post('/api/portfolio/', async (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  // Decrypt the password to check if they match.
+  const user = mysql.conn.query(`SELECT * FROM Users WHERE username='${username}'`, async (err, results) => {
+    if (err) throw err;
+    console.log(results);
+    const matchPassword = await bcrypt.compare(password.toString(), results[0].password.toString());
+    if (matchPassword) {
+      console.log("Logged in!!!");
+    }
+  });
+});
+
+// Inserting a new user into database
+router.put('/api/portfolio/', async (req, res) => {
+  let username = req.body.username;
+  // Check if the username already exists
+  mysql.conn.query(`SELECT * FROM Users WHERE Username = '${username}'`, async (err, results) => {
+    if (err) throw err;
+    if (results > 0) {
+      console.log("Username already exists!");
+    }
+    if(results <= 0) {
+      // Encrypt the password
+      let password = bcrypt.hashSync(req.body.password, 10);
+      mysql.conn.query(`INSERT INTO Users (username, password) VALUES ('${username}', '${password}')`, async (err, results) => {
+        if (err) throw err;
+        console.log(results);
+      });
+    }
+  });
+});
+
 
 module.exports = router;
