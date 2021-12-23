16c7041e45e4ee6647b8a5697285246ca0732cb9
Andrew Kerr <andrewkerr3956@gmail.com>
2021-12-23 14:08:24 -0500

    Minor fixes, also commented out a lot of the functionality to make it more readable.

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.get('/', function (req, res, next) {
   mysql.conn.connect();
 });
 
+// Route to retrieve the Financial Data from Yahoo Stock Prices API
 router.get('/api/search/:symbol', async (req, res) => {
   const error = "Data for this symbol could not be found."
   try {
@@ -26,15 +27,50 @@ router.get('/api/search/:symbol', async (req, res) => {
   }
 });
 
+// Route to create a chart with Chart.js using data retrieved from the Yahoo Stock Prices API
 router.get('/api/chart/:symbol', async (req, res) => {
   const today = new Date();
-  let history = await yahoo.getHistoricalPrices(today.getMonth() - 3, 1, today.getFullYear(), today.getMonth(), today.getDay(),
+  const startMonth = today.getMonth() - 3; // startMonth will be 3 months prior to the current month.
+  const startYear = today.getFullYear();
+  // This conditional will make sure that the current month cannot be less than 0 (January).
+  if(startMonth < 0) {
+    if(startMonth === -1) {
+      // If the startMonth is 1 behind January, it becomes December. Also, the year is not the current year, so it's the year before.
+      startMonth = 11;
+      startYear = today.getFullYear() - 1;
+    }
+    else if(startMonth === -2) {
+      // If the startMonth is 2 behind January, it becomes November. Also, the year is not the current year, so it's the year before.
+      startMonth = 10;
+      startYear = today.getFullYear() - 1;
+    }
+    else if(startMonth === -3) {
+      // If the startMonth is 3 behind January, it becomes October. Also, the year is not the current year, so it's the year before.
+      startMonth = 9;
+      startYear = today.getFullYear() - 1;
+    }
+    else {
+      // If all else fails somehow, the startMonth will be January.
+      startMonth = 0;
+    }
+  }
+  const endMonth = today.getMonth() + 1 /* The +1 is needed here since the endMonth parameter of the API will not return any data from that month 
+  (we want to display data for the current month as well). */
+
+  // This conditional will make sure that the current month cannot be less than 0 (January).
+  if(endMonth > 11) {
+    // If the end month is greater than 11 (December), set it to 11.
+    endMonth = 11;
+  }
+
+  let history = await yahoo.getHistoricalPrices(startMonth, 1, today.getFullYear(), endMonth, today.getDay(),
     today.getFullYear(), req.params.symbol, "1wk");
-  history.shift();
+  history.shift(); // This chops off an extra piece of data that would cause problems if it were there. 
   let historyPrice = [];
   history.map((item) => {
     console.log("the item: ", item);
     if (item.adjclose !== undefined) {
+      // For some stocks, sometimes the price will come up as undefined, we will remove those so the chart doesn't display a gap.
       historyPrice.push(item.adjclose);
     }
   })
@@ -42,16 +78,17 @@ router.get('/api/chart/:symbol', async (req, res) => {
   history.map((item) => {
     console.log("item.date: ", item.date)
     if (item.adjclose !== undefined) {
+      // For some stocks, sometimes the price will come up as undefined, we will remove the dates of those so the chart doesn't display a gap.
       let newDate = new Date(item.date * 1000);
       let formatDate = moment(newDate);
-      formatDate = formatDate.format("YYYY-MM-DD");
-      console.log("newDate: ", formatDate)
+      formatDate = formatDate.format("YYYY-MM-DD"); // Date will look a lot cleaner.
       historyDate.push(formatDate);
     }
   });
-  console.log(historyDate);
-  let displayDates = historyDate.reverse();
+  // The dates and prices of the stocks show up from more recent to older. So we reverse that here so it's older to current, so the chart makes more sense. 
+  let displayDates = historyDate.reverse(); 
   let displayPrices = historyPrice.reverse();
+  // This is where the chart is drawn.
   let thisChart = chartRender.myCanvas
   let stockChart = new chart.Chart(thisChart, {
     type: 'line',
@@ -87,12 +124,14 @@ router.get('/api/chart/:symbol', async (req, res) => {
       }
     }
   });
+  // We're converting the drawn chart to an Image.
   let img = new canvas.Image();
-  img = thisChart.toDataURL();
+  img = thisChart.toDataURL(); // This makes it work with the src attribute of the HTML img element.
   res.send({ img });
-  stockChart.destroy();
+  stockChart.destroy(); // Chart.js requires that we destroy the instance so that it can draw a new one when another symbol is searched.
 });
 
+// Route to retrieve the user's portfolio upon request based on userid.
 router.get('/api/portfolio/:userid', async (req, res) => {
   let userid = req.params.userid;
   mysql.conn.query(`SELECT * FROM vw_UserPortfolio WHERE userid=${userid} `, async (err, results) => {
@@ -107,7 +146,8 @@ router.get('/api/portfolio/:userid', async (req, res) => {
   })
 })
 
-// Checking if the credentials match when you login.
+// Route to check if the credentials match when you login.
+// POST request was used here since GET requests are not ideal for dealing with sensitive information.
 router.post('/api/portfolio/', async (req, res) => {
   let username = req.body.username;
   let password = req.body.password;
@@ -115,12 +155,11 @@ router.post('/api/portfolio/', async (req, res) => {
   mysql.conn.query(`SELECT * FROM Users WHERE username='${username}'`, async (err, results) => {
     if (err) throw err;
     const error = "Invalid login information."
-    console.log(results);
     if (results.length > 0) {
+      // Compare the encrypted password first.
       let matchPassword = await bcrypt.compare(password.toString(), results[0].password.toString());
-      if (matchPassword) {
-        console.log("Logged in!!!");
-        console.log(`Retrieving portfolio at ${results[0].userid}...`);
+      if (matchPassword) { // Passwords match
+        // Retrieve from a view I made in the database that joins the users and portfolio tables on portfolioid. User and portfolio id will be the same.
         mysql.conn.query(`SELECT * FROM vw_UserPortfolio WHERE userid = ${results[0].userid}`, async (err, results) => {
           if (err) throw err;
           if (results > 0) {
@@ -142,6 +181,7 @@ router.post('/api/portfolio/', async (req, res) => {
 });
 
 // Inserting a new user into database
+// PUT was used here because as seen above, since I used a POST request to handle sensitive information using the same route.  
 router.put('/api/portfolio/', async (req, res) => {
   let username = req.body.username;
   // Check if the username already exists
@@ -158,6 +198,7 @@ router.put('/api/portfolio/', async (req, res) => {
         if (err) throw err;
         console.log(results);
       });
+      // I have my database automatically create a new portfolio when a user is made, this just matches the ids in the users table so they will be able to be joined.
       mysql.conn.query(`UPDATE Users SET portfolioid=LAST_INSERT_ID() WHERE userid=LAST_INSERT_ID()`, async (err, results) => {
         if (err) throw err;
         console.log(results);
@@ -166,21 +207,22 @@ router.put('/api/portfolio/', async (req, res) => {
   });
 });
 
+// Route used to save the portfolio.
 router.put('/api/portfolio/save', async (req, res) => {
   const error = "Portfolio could not be saved."
   let collection = req.body.portfolioData
   let wallet = req.body.currentWallet
   console.log(collection);
+  // Update the user's collection and wallet in their portfolio
   mysql.conn.query(`UPDATE Portfolio SET collection = '${JSON.stringify(collection)}', wallet=${wallet} WHERE portfolioid = ${req.body.portfolioId}`, async (err, results) => {
     if (err) throw err;
-    console.log(results);
     if (results.changedRows > 0) {
+      // We use changedRows because that tells us if anything was actually updated.
       res.send({ results });
     }
     else {
       res.send({ error });
     }
-    console.log(results)
   })
 });
 
